f7428b530abf2b6d2e351976b5fdb5ef5008760b
Fynatrium <ronaldoali76326@gmail.com>
2026-08-05 22:39:20 +0330

    Update __init__.py

--- scene_tools/house_type_modeling/__init__.py
+++ scene_tools/house_type_modeling/__init__.py
@@ -1,30 +1,13 @@
+from . import draw_wall_properties
+from . import draw_wall_modifier
 from . import draw_wall
-# from . import edit_wall
-# from . import insert_window
-# from . import insert_door
-# from . import wall_marking
-# from . import arrange_switches
-# from . import create_ground
-# from . import create_ceiling
-
 
 def register():
+    draw_wall_properties.register()
+    draw_wall_modifier.register()
     draw_wall.register()
-    # edit_wall.register()
-    # insert_window.register()
-    # insert_door.register()
-    # wall_marking.register()
-    # arrange_switches.register()
-    # create_ground.register()
-    # create_ceiling.register()
-
 
 def unregister():
-    # create_ceiling.unregister()
-    # create_ground.unregister()
-    # arrange_switches.unregister()
-    # wall_marking.unregister()
-    # insert_door.unregister()
-    # insert_window.unregister()
-    # edit_wall.unregister()
     draw_wall.unregister()
+    draw_wall_modifier.unregister()
+    draw_wall_properties.unregister()
