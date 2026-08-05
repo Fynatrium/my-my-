5e90dd537164d43433b548650d69928ab9ab33d1
Fynatrium <ronaldoali76326@gmail.com>
2026-08-05 21:21:21 +0330

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,14 +9,16 @@ bl_info = {
 }
 
 import bpy
+import importlib
+import sys
 
-# Tool options FIRST (properties must exist before operators use them)
+if "bpy" in locals():
+    for mod_name in list(sys.modules.keys()):
+        if mod_name.startswith(__name__ + "."):
+            importlib.reload(sys.modules[mod_name])
+
 from . import tool_options
-
-# Scene tools (operators)
 from .scene_tools import house_type_modeling
-
-# Panels
 from . import scene_panel
 from . import account_panel
 from . import often_panel
@@ -26,11 +28,9 @@ from . import report_production_panel
 from . import product_library_panel
 from . import backend_panel
 
-
 def register():
     tool_options.register()
     house_type_modeling.register()
-    
     scene_panel.register()
     account_panel.register()
     often_panel.register()
@@ -40,20 +40,17 @@ def register():
     product_library_panel.register()
     backend_panel.register()
 
-
 def unregister():
     backend_panel.unregister()
     product_library_panel.unregister()
     report_production_panel.unregister()
     cabinet_panel.unregister()
-    scene_panel.unregister()
     global_panel.unregister()
     often_panel.unregister()
     account_panel.unregister()
-    
+    scene_panel.unregister()
     house_type_modeling.unregister()
     tool_options.unregister()
-
 
 if __name__ == "__main__":
     register()
